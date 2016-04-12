90e9e156ca9652555a29469e31e8a5fc41f9bf06
Andras Kupcsik <kupcsika@gmail.com>
2016-04-12 13:35:50 +0800

    addig a dummy frame file

---
+++ frame.m
@@ -0,0 +1,20 @@
+clear all, close all
+
+% start robot, robot waits for confirmation from matlab to start
+
+% send the robot the confirmation that matlab is running
+
+% robot waits for context estimation
+
+% ==== VERY FAST ====
+% robot sends context to matlab, matlab samples controller from policy
+
+% robot sends the controller parameter
+% ===================
+
+% experiment is executed
+
+% robot sends measurement data to matlab, shuts down
+
+% human gives feedback (real feedback, or failed experiment)
+
