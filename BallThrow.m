1eacd41e11e0cf6e37e913086acfd93cb139af2c
Peter Karkus <karkus@Peters-MacBook-Pro.local>
2016-06-29 16:20:00 +0800

    Added receiving trajectory from KUKA server. Not tested

--- BallThrow.m
+++ BallThrow.m
@@ -2,6 +2,8 @@ addpath('Matlab_network');
 
 ip = '172.31.1.147';
 port = 30000;
+z_tresh = 100; %100mm over ground
+target_coord = [5000, 5000];
 
 %% set up tcp client
 tcpClient = tcpip(ip, port, 'NetworkRole','Client');
@@ -34,6 +36,28 @@ while 1
     
     tcp_runexperiment(tcpClient, theta)
 
+    % receive trajectory, will wait until timeout
+    % should be in the format [x y z alpha beta theta], mm and rad
+    % Z is height, positive up
+    tr = tcp_getdata(tcpClient);
+    
+    % compute coords of hitting the ground (first gets under z treshold)
+    first_ind = find(tr(:,3) < z_tresh, 1);
+    if ~first_ind
+        fprintf('Never went under treshold %f. Closest was %f.', z_tresh, min(tr(:,3)));
+    end
+    hit_coord = tr(first_ind, :);
+    diff2target = hit_coord - target_coord;
+        
+    % plot trajectory
+    figure(1);
+    stem3(tr(:,1), tr(:,2), tr(:,3));
+    hold on;
+    stem3(hit_coord(1), hit_coord(2), hit_coord(3), '-r');
+    hold off;
+    
+    fprintf('Difference to target: [%f, %f], %f', diff2target(1), diff2target(2), norm(diff2target))
+    
     % get score manually from user
     string = '';
     while 1
