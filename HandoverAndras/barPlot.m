cd42d192f2d9f905e3a79e78b2b749734ab3ba45
Andras Kupcsik <jani@huifeis-MacBook-Pro.local>
2016-12-09 17:05:11 +0800

    barPlot

--- HandoverAndras/barPlot.m
+++ HandoverAndras/barPlot.m
@@ -11,9 +11,9 @@ for i = 1:length(user.names)
     learntMean = data.meanR_learntMean(end)* 9/4 + 5.5;
     initMean = data.meanR_initMean(end)*9/4 + 5.5;
     barData(i, :) = [learntMean, mean(user.finalScores(i, :)), initMean, mean(user.initScores(i, :))];
-    errorBarData(i, 1) = data.stdR_learntMean(end)*2*(9/4);
+    errorBarData(i, 1) = data.stdR_learntMean(end)*4*(9/4);
     errorBarData(i, 2) = std(user.finalScores(i, :));
-    errorBarData(i, 3) = data.stdR_initMean(end)*2*(9/4);
+    errorBarData(i, 3) = data.stdR_initMean(end)*4*(9/4);
     errorBarData(i, 4) = std(user.initScores(i, :));
     
     
