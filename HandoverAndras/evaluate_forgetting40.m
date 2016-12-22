da22548262aec5fab4adecef86f4aae352d47cda
Andras Kupcsik <jani@huifeis-MacBook-Pro.local>
2016-12-23 07:49:56 +0800

    eval update

--- HandoverAndras/evaluate_forgetting40.m
+++ HandoverAndras/evaluate_forgetting40.m
@@ -12,7 +12,7 @@ ridge = 1e-4;
 fixedActivation = 0.2;
 
 userData;
-
+figure
 for j =1:length(user.names)
     load(['HandoverLearningOrientation_', user.names{j}, '.mat'])
     
@@ -23,6 +23,7 @@ for j =1:length(user.names)
     dpol(j) = data.diffPolicy(end);
     dpolmean(j) = data.diffPolicyMean(end);
     meanKldiv(j) = mean(data.KLdiv);
+    diffRModel(j) = data.meanR(end) - data.meanR_init(end);
     
     if diffReported(j) > 0
         marker{j} = 'o';
@@ -32,25 +33,45 @@ for j =1:length(user.names)
         marker{j} = 'x';
     end
     
-    figure(1)
+    subplot(5, 2, 1)
      plot(diffReported(j),kldiv(j),  marker{j}), hold on
      ylabel('kl div'), xlabel('diff R reported')
      
-     figure(2) 
+     subplot(5, 2, 3)
      plot(diffReported(j), corr(j), marker{j}), hold on
      ylabel('corr policy mean'), xlabel('diff R reported')
      
-     figure(3) 
+     subplot(5, 2, 5)
      plot(diffReported(j), dpol(j), marker{j}), hold on
      ylabel('diff final policy R'), xlabel('diff R reported')
      
-     figure(4) 
+     subplot(5, 2, 7)
      plot(diffReported(j), dpolmean(j), marker{j}), hold on
      ylabel('diff final policy mean R'), xlabel('diff R reported')
      
-     figure(5)
+     subplot(5, 2, 9)
      plot(diffReported(j),meanKldiv(j),  marker{j}), hold on
      ylabel('mean kl div'), xlabel('diff R reported')
+     
+      subplot(5, 2, 2)
+     plot(diffRModel(j),kldiv(j),  marker{j}), hold on
+     ylabel('kl div'), xlabel('diff R model')
+     
+     subplot(5, 2, 4)
+     plot(diffRModel(j), corr(j), marker{j}), hold on
+     ylabel('corr policy mean'), xlabel('diff R model')
+     
+     subplot(5, 2, 6)
+     plot(diffRModel(j), dpol(j), marker{j}), hold on
+     ylabel('diff final policy R'), xlabel('diff R model')
+     
+     subplot(5, 2, 8)
+     plot(diffRModel(j), dpolmean(j), marker{j}), hold on
+     ylabel('diff final policy mean R'), xlabel('diff R model')
+     
+     subplot(5, 2, 10)
+     plot(diffRModel(j),meanKldiv(j),  marker{j}), hold on
+     ylabel('mean kl div'), xlabel('diff R model')
     
 end
 
