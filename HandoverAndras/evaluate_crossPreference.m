ff56d80202906dc58c99c12419f6bc8a75735bd0
Andras Kupcsik <jani@huifeis-MacBook-Pro.local>
2017-01-25 13:11:22 +0800

    eval update

--- HandoverAndras/evaluate_crossPreference.m
+++ HandoverAndras/evaluate_crossPreference.m
@@ -20,27 +20,27 @@ end
 for j =1:length(user.names)
     load(['HandoverLearningOrientation_', user.names{j}, '.mat'])
     
-    hypFinal = data.hyp40(end, :); 
+    hypFinal = data.hyp(end, :); 
     sig = hypFinal(1);
     sigma2 = hypFinal(2);
     w = hypFinal(3:end); W = diag(w.^-2);
     x = data.samples;
     prefs = data.prefFeedback;
     absFeedback = data.absFeedback;
+    absFeedback(:, 2) = (data.absFeedback(:, 2) - 5.5)/9*4;
     
     Sigma = exp(-.5 * maha(x, x, W)) ;
     Sigma = Sigma + eye(size(Sigma)) * ridge;
     
     f = zeros(size(x,1), 1);
     [fmap, ddS, GammaMap] = nr_plgp_wPrior(f, prefs, Sigma, sig, absFeedback, sigma2);
-    
-    % Sample a lot for policy update
+   
     iK = eye(size(Sigma))/(Sigma);
     
     for i = 1:size(finalPolicyMean, 1)
         xsampled = finalPolicyMean(i, :);
        
-        kall = exp(-.5 * maha(xsampled, x, W))
+        kall = exp(-.5 * maha(xsampled, x, W));
         Kxx = exp(-.5 * maha(xsampled, xsampled, W)) ;
         SigmaStar = ridge* eye(size(Kxx)) + Kxx - kall / (Sigma + eye(size(GammaMap))/(GammaMap + ridge*eye(size(Sigma)))) * kall';
         SigmaStar = (SigmaStar + SigmaStar')/2;
@@ -49,5 +49,29 @@ for j =1:length(user.names)
         
         meanFinalRew(j, i) = ypred;
         stdFinalRew(j, i) = SigmaStar.^.5;
+        kernelAct(j, i) = mean(kall);
     end
+    
+    ix = [1:j-1, j+1:10];
+    
+    prefRateMean(j, :) = (meanFinalRew(j, j) - meanFinalRew(j, ix)) *9/4 ;
+    prefRateStd(j, :) = (stdFinalRew(j, j)^2 + stdFinalRew(j, ix).^2).^.5 * 9/4;
 end
+
+barData(:, 1) = mean(prefRateMean , 2);
+errorBarData(:, 1) = (sum(prefRateStd.^2, 2)/9^2).^.5;
+
+barData2(:, 1) = mean(kernelAct);
+errorBarData2(:,1) = 4*std(kernelAct);
+
+figure,  h = barwitherr(4*errorBarData, barData);
+xlabel('#subject')
+ylabel('Diff Reward')
+title('Reward Advantage')
+set(gca,'box','off')
+set(h(1), 'FaceColor', 'y')
+
+figure,  barwitherr(4*errorBarData2, barData2)
+ylabel('kernel activation')
+
+corrcoef([barData, barData2])
